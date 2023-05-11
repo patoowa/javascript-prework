a8a17e1b2bd3b74ea1da4fdfea25c61b9264a3da
Ola Popiołek <patoowa@gmail.com>
2023-05-11 16:58:23 +0200

    Add math floor, random, if statement

--- js/script.js
+++ js/script.js
@@ -1,3 +1,74 @@
 let playerMove;
 playerMove = 'papier';
-printMessage ('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+printMessage ('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+
+let computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log(`Wylosowana liczba to: ${randomNumber}`);
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+printMessage(`Mój ruch: ${computerMove}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
